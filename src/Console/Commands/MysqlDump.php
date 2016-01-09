2f99eb914c9d26934b72987c97ca8eca9b7a031a
Paul Vidal <i@paulvl.me>
2016-01-09 11:54:12 -0500

    Applied fixes from StyleCI

--- src/Console/Commands/MysqlDump.php
+++ src/Console/Commands/MysqlDump.php
@@ -7,11 +7,11 @@ use Illuminate\Support\Facades\Storage;
 
 class MysqlDump extends Command
 {
-     /**
-    * The name and signature of the console command.
-    *
-    * @var string
-    */
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
     protected $signature = 'backup:mysql-dump
                             {filename? : Mysql backup filename}';
 
@@ -29,33 +29,31 @@ class MysqlDump extends Command
      */
     public function handle()
     {
-        $host           = config('database.connections.mysql.host');
-        $database       = config('database.connections.mysql.database');
-        $username       = config('database.connections.mysql.username');
-        $password       = config('database.connections.mysql.password');
+        $host = config('database.connections.mysql.host');
+        $database = config('database.connections.mysql.database');
+        $username = config('database.connections.mysql.username');
+        $password = config('database.connections.mysql.password');
 
-        $backupPath     = config('backup.mysql.path');
+        $backupPath = config('backup.mysql.path');
 
-        $cloudStorage   = config('backup.mysql.cloud-storage.enabled');
-        $cloudDisk      = config('backup.mysql.cloud-storage.disk');
-        $cloudPath      = config('backup.mysql.cloud-storage.path');
-        $keepLocal      = config('backup.mysql.cloud-storage.keep-local');
+        $cloudStorage = config('backup.mysql.cloud-storage.enabled');
+        $cloudDisk = config('backup.mysql.cloud-storage.disk');
+        $cloudPath = config('backup.mysql.cloud-storage.path');
+        $keepLocal = config('backup.mysql.cloud-storage.keep-local');
 
-        $filename       = $database . '_' . empty(trim($this->argument('filename'))) ? $database.'_'.\Carbon\Carbon::now()->format('YmdHis') : trim($this->argument('filename'));
+        $filename = $database.'_'.empty(trim($this->argument('filename'))) ? $database.'_'.\Carbon\Carbon::now()->format('YmdHis') : trim($this->argument('filename'));
 
-        $dumpCommand    = "mysqldump -e -f -h $host -u $username -p$password $database > $backupPath$filename.sql";
+        $dumpCommand = "mysqldump -e -f -h $host -u $username -p$password $database > $backupPath$filename.sql";
 
         exec($dumpCommand);
 
         $this->info('Mysql backup completed!');
 
-        if($cloudStorage)
-        {
+        if ($cloudStorage) {
             $fileContents = file_get_contents("$backupPath$filename.sql");
             Storage::disk($cloudDisk)->put("$cloudPath$filename.sql", $fileContents);
 
-            if(!$keepLocal)
-            {
+            if (!$keepLocal) {
                 $rmCommand = "rm $backupPath$filename.sql";
                 exec($rmCommand);
             }
